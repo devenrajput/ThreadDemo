96cd07797775cd1b59ec815e9e806c1293fa2841
Devendra Kumar <dk1721080@gmail.com>
2024-03-24 01:23:46 +0530

    Thread start

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,16 +1,43 @@
 package ThreadDemo;
 
-class MyFirstThread extends Thread{
+class Table{
+    synchronized void printTable(int x){
+        for(int i=1;i<=10;i++){
+            System.out.println(i*x);
+        }
+    }
+}
+
+class FirstThread extends Thread{
+    Table t;
+    public FirstThread(Table t) {
+        this.t=t;
+    }
+
     public void run(){
-        System.out.println("Hii everyone");
+       t.printTable(2);
+    }
+}
+
+class SecondThread extends Thread{
+    Table t;
+    public SecondThread(Table t) {
+        this.t=t;
+    }
+
+    public void run(){
+        t.printTable(3);
     }
 }
 
 public class ThreadTest{
     public static void main(String args[]){
 
-        MyFirstThread  threadObj = new MyFirstThread();
+        Table t = new Table();
+        FirstThread  threadObj = new FirstThread(t);
         threadObj.start();
+        SecondThread  threadObj1 = new SecondThread(t);
+        threadObj1.start();
 
     }
 }
