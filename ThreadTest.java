b653c7b61c306c0d6f9178507ce84193c8349a05
Devendra Kumar <dk1721080@gmail.com>
2024-03-24 01:12:55 +0530

    Thread obj create and thread start

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,9 +1,16 @@
 package ThreadDemo;
 
 class MyFirstThread extends Thread{
-    
+    public void run(){
+        System.out.println("Hii everyone");
+    }
 }
 
 public class ThreadTest{
+    public static void main(String args[]){
 
+        MyFirstThread  threadObj = new MyFirstThread();
+        threadObj.start();
+
+    }
 }
